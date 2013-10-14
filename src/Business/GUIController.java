e74da0ac66d0c3d24f5ebcffbd3a966bb5f30b3a
Harvey Sandí <HMSScr@gmail.com>
2013-10-13 22:23:43 -0600

    document

--- src/Business/GUIController.java
+++ src/Business/GUIController.java
@@ -96,27 +96,52 @@ public class GUIController {
 	/**
 	 * Method that adds a new subject to the semester.
 	 * @param subject Subject to be added.
-	 * @param semester Semester where the subject will be aded.
+	 * @param semester Semester where the subject will be added.
 	 */
 	public void addSubjectToSemester(String subject, String semester) {
 		searchSemester(Byte.valueOf(semester)).addSubject(searchSubject(subject));
 	}
-
+	
+	/**
+	 * Method that adds a new student to users database.
+	 * @param identification Identification of the new student.
+	 * @param name Name of the student.
+	 * @param password Password to access the student
+	 */
 	public void addStudent(String identification, String name, String password) {
 		this.users.add(new Student(identification, name, password));
 	}
 
+	/**
+	 * Method that adds a new professor to users database.
+	 * @param password Password to access the professor.
+	 * @param identification Identification of the student.
+	 * @param name Name of the student.
+	 * @param department Department which professor belongs.
+	 */
 	public void addProfessor(String password, String identification,
 			String name, String department) {
 		this.users.add(new Professor(password, identification, name,
 				searchDepartment(department)));
 	}
 
+	/**
+	 * Method that adds a new coordinator to users database.
+	 * @param password Password to access the coordinator.
+	 * @param identification identification of the coordinator.
+	 * @param name Name of the coordinator.
+	 */
 	public void addCoordinator(String password, String identification,
 			String name) {
 		this.users.add(new Coordinator(password, identification, name));
 	}
 
+	/**
+	 * Method that assigns a new group of the subject to the professor and assigns the professor to the subject.
+	 * @param professor Professor identification.
+	 * @param subject Subject name.
+	 * @param groupNumber Number of the group.
+	 */
 	public void assignGroup(String professor, String subject, String groupNumber) {
 		Professor professorResult = searchProfessor(professor);
 		Subject subjectResult = searchSubject(subject);
@@ -124,32 +149,76 @@ public class GUIController {
 		subjectResult.addProfessor(professorResult);
 	}
 
+	/**
+	 * Method that adds a new lesson to a specific professor group.
+	 * @param day  Day to receive the lesson.
+	 * @param begin Time to start lesson.
+	 * @param end  Time  to end lesson.
+	 * @param group Number of the group.
+	 * @param subject Name of the subject.
+	 */
 	public void addLesson(String day, String begin, String end, String group, String subject) {
 		((Professor) userSession).addLesson(day.charAt(0), begin.charAt(0), end.charAt(0),
 				Integer.parseInt(group),subject);
 	}
 
+	/**
+	 * Method that adds a new lesson to a specific professor group.
+	 * @param professor Identification of the professor.
+	 * @param day Day to receive the lesson.
+	 * @param begin Time to start lesson.
+	 * @param end Time to end lesson.
+	 * @param group Number of the group.
+	 * @param subject Name of the subject.
+	 */
 	public void addLesson(String professor, String day, String begin, String end, 
 			String group, String subject) {
 		searchProfessor(professor).addLesson(day.charAt(0),begin.charAt(0),end.charAt(0),
 				Integer.parseInt(group),subject);
 	}
 
+	/**
+	 * Method that remove one lesson.
+	 * @param day Day that receive the lesson.
+	 * @param begin Time that start the lesson.
+	 * @param end Time of the end lesson.
+	 * @param group Number of the group.
+	 * @param subject Name of the subject.
+	 */
 	public void removeLesson(String day, String begin, String end, String group, String subject) {
 		((Professor) userSession).removeLesson(day.charAt(0),begin.charAt(0),end.charAt(0),
 				Integer.parseInt(group),subject);
 	}
-
+	
+	/**
+	 * Method that remove one lesson.
+	 * @param professor Identification of the professor
+	 * @param day Day that receive the lesson.
+	 * @param begin Time that start the lesson.
+	 * @param end Time of the end lesson.
+	 * @param group Number of the group.
+	 * @param subject Name of the subject.
+	 */
 	public void removeLesson(String professor, String day, String begin, String end,
 			String group, String subject) {
 		searchProfessor(professor).removeLesson(day.charAt(0),begin.charAt(0),end.charAt(0),
 				Integer.parseInt(group),subject);
 	}
 	
+	/**
+	 * Method that generate the schedule by information of the database.
+	 * @param semester Number of the semester.
+	 * @return Returns the errors found during the building process
+	 */
 	public ArrayList<String> buildSemesterSchedule(String semester) {
 		return searchSemester(Byte.valueOf(semester)).buildSchedule(classRooms);
 	}
 	
+	/**
+	 * Method that adds a Technical Equipment to classroom.
+	 * @param technicalEquipment One or more technical equipment to add to a classroom.
+	 * @param classRoom Name of the classroom.
+	 */
 	public void addTechnicalEquipmentToClassRoom(String[] technicalEquipment, String classRoom) {
 		PracticalClassroom resultClassRoom = ((PracticalClassroom)searchClassRoom(classRoom));
 		for (int t = 0; t < technicalEquipment.length; t++) {
@@ -157,6 +226,11 @@ public class GUIController {
 		}
 	}
 	
+	/**
+	 * Method that adds a support material to a subject.
+	 * @param supportMaterial One or more support material to add to a subject.
+	 * @param subject Name of the subject.
+	 */
 	public void addSupportMaterialToSubject(String[] supportMaterial, String subject) {
 		Practical resultSubject = ((Practical)searchSubject(subject));
 		for (int s = 0; s < supportMaterial.length; s++) {
@@ -164,6 +238,12 @@ public class GUIController {
 		}
 	}
 
+	/**
+	 * Method that make a identification and password validation of the user.
+	 * @param login Identification of the user.
+	 * @param password Password of the user.
+	 * @return Returns the user.
+	 */
 	public Person login(String login, String password) {
 		for (int i = 0; i < users.size(); i++) {
 			if ((users.get(i).getIdentification().equals(login))
@@ -175,6 +255,11 @@ public class GUIController {
 		return null;
 	}
 
+	/**
+	 * Method that search a department.
+	 * @param department Name of the department.
+	 * @return Returns the department.
+	 */
 	public Department searchDepartment(String department) {
 		for (int i = 0; i < departments.size(); i++) {
 			if (departments.get(i).getName().equals(department))
@@ -183,6 +268,11 @@ public class GUIController {
 		return null;
 	}
 
+	/**
+	 * Method that search a classroom.
+	 * @param classRoom Name of the classroom.
+	 * @return Return the classroom.
+	 */
 	public ClassRoom searchClassRoom(String classRoom) {
 		for (int i = 0; i < classRooms.size(); i++) {
 			if (classRooms.get(i).getName().equals(classRoom))
@@ -191,6 +281,11 @@ public class GUIController {
 		return null;
 	}
 
+	/**
+	 * Method that search a professor by identification.
+	 * @param professor Identification of the professor.
+	 * @return Returns the professor.
+	 */
 	public Professor searchProfessor(String professor) {
 		for (int i = 0; i < users.size(); i++) {
 			if (users.get(i).getIdentification().equals(professor))
@@ -199,6 +294,11 @@ public class GUIController {
 		return null;
 	}
 	
+	/**
+	 * Method that search a professor by name.
+	 * @param professor Name of the professor.
+	 * @return Returns the professor.
+	 */
 	public Professor searchProfessorbyName(String professor) {
 		for (int i = 0; i < users.size(); i++) {
 			if (users.get(i).getName().equals(professor))
@@ -207,6 +307,11 @@ public class GUIController {
 		return null;
 	}
 
+	/**
+	 * Method that search a professor.
+	 * @param semester Number of the semester.
+	 * @return Return the semester.
+	 */
 	public Semester searchSemester(byte semester) {
 		for (int i = 0; i < semesters.size(); i++) {
 			if (semesters.get(i).getNumber() == semester)
@@ -215,6 +320,11 @@ public class GUIController {
 		return null;
 	}
 
+	/**
+	 * Method that search a subject.
+	 * @param subject Name of the subject.
+	 * @return Returns the subject.
+	 */
 	public Subject searchSubject(String subject) {
 		for (int i = 0; i < subjects.size(); i++) {
 			if (subjects.get(i).getName().equals(subject))
@@ -223,6 +333,11 @@ public class GUIController {
 		return null;
 	}
 	
+	/**
+	 * Method that search a technical equipment.
+	 * @param technicalEquipment Name of the technical equipment.
+	 * @return Returns the equipment.
+	 */
 	public TechnicalEquipment searchTechnicalEquipment(String technicalEquipment) {
 		for (int i = 0; i < this.technicalEquipment.size(); i++) {
 			if (this.technicalEquipment.get(i).getName().equals(technicalEquipment))
@@ -231,6 +346,11 @@ public class GUIController {
 		return null;
 	}
 	
+	/**
+	 * Method that search a support material. 
+	 * @param supportMaterial Name of the support material.
+	 * @return Returns the support material.
+	 */
 	public SupportMaterial searchSupportMaterial(String supportMaterial) {
 		for (int i = 0; i < this.supportMaterial.size(); i++) {
 			if (this.supportMaterial.get(i).getName().equals(supportMaterial))
@@ -239,42 +359,82 @@ public class GUIController {
 		return null;
 	}
 	
+	/**
+	 * Method that adds new technical equipment database. 
+	 * @param name Name of the technical equipment.
+	 * @param amount Amount of the technical equipment.
+	 */
 	public void addTechnicalEquipment(String name, String amount) {
 		this.technicalEquipment.add(new TechnicalEquipment(name,Integer.parseInt(amount)));
 	}
 	
+	/**
+	 * Method that adds a new support material database.
+	 * @param name Name of the support material.
+	 */
 	public void addSupportMaterial(String name) {
 		this.supportMaterial.add(new SupportMaterial(name));
 	}
 	
+	/**
+	 * Method that gets the classroom database.
+	 * @return Returns the classroom database.
+	 */
 	public ArrayList<ClassRoom> getClassRooms() {
 		return this.classRooms;
 	}
 	
+	/**
+	 * Method that gets the semester database.
+	 * @return Returns the semester database.
+	 */
 	public ArrayList<Semester> getSemesters() {
 		return this.semesters;
 	}
 	
+	/**
+	 * Method that gets the subject database.
+	 * @return Returns the subject database.
+	 */
 	public ArrayList<Subject> getSubjects() {
 		return this.subjects;
 	}
 	
+	/**
+	 * Method that gets the department database.
+	 * @return Returns the department database.
+	 */
 	public ArrayList<Department> getDepartments() {
 		return this.departments;
 	}
 	
+	/**
+	 * Method that gets the technical equipment database.
+	 * @return Returns the technical equipment database.
+	 */
 	public ArrayList<TechnicalEquipment> getTechnicalEquipment() {
 		return this.technicalEquipment;
 	}
 	
+	/**
+	 * Method that gets the support material database.
+	 * @return Returns the support material database.
+	 */
 	public ArrayList<SupportMaterial> getSupportMaterial() {
 		return this.supportMaterial;
 	}
 	
+	/**
+	 * Method that gets the users database.
+	 * @return Returns the users database.
+	 */
 	public ArrayList<Person> getPerson(){
 		return this.users;
 	}
 
+	/**
+	 * Method that adds new information of the database.
+	 */
 	public void initialize() {
 		this.addClassRoom("Windows", "Prac1", "Este", "30", "1");
 		this.addClassRoom("Windows", "Prac2", "Este", "30", "2");
